0997f08068a5eb9a7b3165c47b9407180f806880
Matt Roberts <matt.roberts@getstream.io>
2021-10-13 14:47:25 -0600

    create file and start of read

--- create.js
+++ create.js
@@ -1,45 +1,43 @@
-const express = require('express');
-const cors = require('cors');
 const { connect } = require('getstream');
 
-
-
 require('dotenv').config({path: './.env'});
-
-
-const app = express();
-const port = 5000;
 
 const key = process.env.API_KEY;
 const secret = process.env.API_SECRET;
 const appId = process.env.APP_ID;
-
-app.use(cors());
-app.use(express.json());
 
 // Instantiate a new client (server side)
 // Optionally supply the app identifier and an options object specifying the data center to use and timeout for requests (15s)
 // const client = connect('YOUR_API_KEY', 'API_KEY_SECRET', 'APP_ID', { location: 'us-east', timeout: 15000 });
 const serverClient = connect(key, secret, appId);
 
-
 // Create a token for user with id "the-user-id"
-const userToken = serverClient.createUserToken('USER ID GOES HERE');
+const userToken = serverClient.createUserToken('eric');
 
-app.get('/', (req, res) => {
-  res.send('Feeds Training');
-})
+const getToken = () => {
+  // const userToken = serverClient.createUserToken('eric')
+   console.log('token', userToken);
+}
 
-app.get('/test', (req, res) => {
-  res.send('Hola Mundo!!!');
-})
+const feedA = serverClient.feed('user', 'feedA')
 
-app.post('/token', (req, res) => {
+const createFeed = () => {
+  
+  console.log('FEED A', feedA);
+}
 
-})
+const createActivity = async () => {
 
+  const activity = {
+    actor: 'user:feedA',
+    verb: 'like',
+    object: 'place:1'
+  };
 
+  await feedA.addActivity(activity).then( r => console.log("activity response", r))
 
-app.listen(port, () => {
-  console.log(`Feeds app listening on port ${port}`);
-})
+}
+
+// getToken();
+// createFeed();
+createActivity();
